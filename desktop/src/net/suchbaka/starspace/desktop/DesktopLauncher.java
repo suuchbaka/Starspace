010b980f2397598bbe10f0a9d00d10c220a7d16d
suuchbaka <suchbaka@gmail.com>
2016-11-24 14:29:12 +0300

    Initial Commit

---
+++ desktop/src/net/suchbaka/starspace/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.suchbaka.starspace.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.suchbaka.starspace.StarspaceGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarspaceGame(), config);
+	}
+}
